ad1c08c5b4581514113103f2fc366e749228a41f
Syed Imran <syedimranhgmrd@gmail.com>
2021-06-09 14:00:18 +0530

    variables and funcions

---
+++ javascript/app.js
@@ -0,0 +1,92 @@
+// console.log(20);
+// console.log(22.684683);
+// console.log("Hello Jspiders");
+// console.log('Hello Jspiders');
+// console.log(`Hello Qspiders`);
+// console.log(true);
+// console.log(null)
+// var a;
+// console.log(a);
+
+// console.log(typeof 20);
+// console.log(typeof 22.684683);
+// console.log(typeof "Hello Jspiders");
+// console.log(typeof 'Hello Jspiders');
+// console.log(typeof `Hello Qspiders`);
+// console.log(typeof true);
+// console.log(typeof null)
+// var a;
+// console.log(typeof a);
+
+//! var,let and const
+
+// var a; //D
+// console.log(a);
+// a=20; //I
+// console.log(a);
+// a="Dinga";//R-I
+// console.log(a);
+
+// var a;//R-D
+
+// console.log(a)
+
+// let a; //D
+// console.log(a);
+// a=20; //I
+// console.log(a);
+// a="Dinga";//R-I
+// console.log(a);
+
+// let a;//R-D not allowed
+
+// console.log(a);
+
+// const a=20; //Both D and  I
+// console.log(a);
+
+// !Scope of Variables
+
+// var a="Dinga";
+
+// if(20==20)
+// {
+//     var a=30;
+//     console.log(a)
+// }
+
+// console.log(a)
+
+// var i="Sheela";
+
+// for(const i=1;i<=10;i++)
+// {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+// function m1(a,b)
+// {
+//     console.log("Hello pinki");
+//     console.log(a+b)
+// }
+
+// m1(4,4);
+
+var a="Dinga";
+
+function m2()
+{
+    var a=20;
+    console.log(a)
+}
+
+m2();
+// let res=m2();
+
+// console.log(res)
+
+// console.log(m2())
+
+console.log(a)
