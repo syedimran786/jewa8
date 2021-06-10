4ea89c671f6f938a4e75d3a14be4e921c371f72f
Syed Imran <syedimranhgmrd@gmail.com>
2021-06-10 13:38:02 +0530

    Arrays

---
+++ array.js
@@ -0,0 +1,64 @@
+// let a=["raja","Rani",676,true,null];
+// let n=[22,444,66,778,99];
+// console.log(a);
+// console.log(n)
+
+// console.log(a[0]);
+// a[5]="Jspiders";
+
+// a[10]="Qspiders";
+
+// console.log(a);
+// console.log(a[7])
+
+// console.log(typeof a);
+
+
+let colors=["red","green","pink","yellow","blue","orange"];
+
+console.log(colors);
+
+// for(let i=0;i<colors.length;i++)
+// {
+//     console.log(colors[i]);
+// }
+
+// for(let color of colors) {
+//     console.log(color)
+// }
+
+
+// let res=colors.push("violet","indigo");
+
+// console.log(colors);
+// console.log(res);
+
+// let res1=colors.unshift("magenta","purple","gray");
+
+// console.log(colors);
+// console.log(res1);
+
+// let res2=colors.pop();
+// console.log(colors);
+
+// console.log(res2);
+
+// let res3=colors.shift();
+// console.log(colors);
+// console.log(res3);
+
+//!let colors=["red","green","pink","yellow","blue","orange"];
+// let res1=colors.splice(3,0,"black","white");
+
+// console.log(colors)
+// console.log(res1)
+
+// let res1=colors.splice(2,3,"black","white");
+
+// console.log(colors)
+// console.log(res1)
+
+let res1=colors.splice(2,1);
+
+console.log(colors)
+console.log(res1)
