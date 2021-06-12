c2dd485a80d1c341ce4706f6f3792cc806589870
Syed Imran <syedimranhgmrd@gmail.com>
2021-06-12 14:02:29 +0530

    strings and objects

---
+++ mini project/objects.js
@@ -0,0 +1,37 @@
+// let students=
+// [
+//     {
+//         sname:"Dinga",
+//         age:24,
+//         dept:"CSE"
+//     },
+//     {
+//         sname:"Sheela",
+//         age:18,
+//         dept:"ISE"
+//     },
+//     {
+//         sname:"Nick",
+//         age:28,
+//         dept:"Mech"
+//     }
+// ];
+
+// // console.log(students[1].sname)
+
+// for(let stud of students) {
+//     console.log(stud.sname)
+// }
+
+
+
+// Popup boxes
+
+// alert("Data Submitted Successfully");
+// let res=confirm("Do you want to allow notifications from this site");
+// console.log(res);
+let a=Number(prompt("Enter value of A"));
+let b=Number(prompt("Enter value of A"));
+let sum=a+b;
+console.log(sum)
+console.log(typeof sum)
---
+++ mini project/strings.js
@@ -0,0 +1,60 @@
+// let s='Hello World';
+
+// console.log(s);
+// console.log(s.length);
+
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+// console.log(s.substr(2,4));
+// console.log(s.substr(1,6));
+
+//! We can use Below methods for arrays also
+// console.log(s.slice(2,4));
+// console.log(s.indexOf("l",4));
+// console.log(s.indexOf("l",2));
+// console.log(s.indexOf("l"));
+// console.log(s.indexOf("z"));
+// console.log(s.lastIndexOf("l"))
+// console.log(s.lastIndexOf("o"))
+// console.log(s.lastIndexOf("z"))
+// console.log(s.includes("l"))
+// console.log(s.includes("z"))
+// console.log(s.concat(" Hello Bangalore"))
+//! We can use Above methods for arrays also
+// console.log(s.startsWith("s"));
+// console.log(s.startsWith("H"));
+// console.log(s.startsWith("h"));
+
+// console.log(s.endsWith("s"));
+// console.log(s.endsWith("d"));
+// console.log(s.endsWith("D"));
+
+// console.log(s.repeat(3))
+
+// console.log(s.replace("l", "X"))
+// console.log(s.replace(/l/g, "X"));
+
+// console.log(s.charAt(4))
+// console.log(s.charAt(8))
+// console.log(s.charCodeAt(4))
+// console.log(s.charCodeAt(8))
+
+// console.log(String.fromCharCode(114))
+// console.log(String.fromCharCode(65))
+// console.log(s.charAt(100))
+
+// m1();
+
+
+console.log(a)
+let a=20;
+console.log(a)
+// function m1()
+// {
+//     // console.log(b)
+//     var b=40;
+//     console.log(b);
+// }
+
+
+
